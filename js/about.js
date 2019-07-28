0129c88d22b7483405e1debe181df067fffee9d4
Liv <w.phrush@gmail.com>
2019-07-28 18:40:21 -0500

    add aboutoptions and functionality to change text

--- js/about.js
+++ js/about.js
@@ -1,12 +1,97 @@
-function changeBackgroundColor (color) {
-  document.getElementsByClassName('about-page')[0].style.backgroundColor = color;
+const aboutOptions = {
+  red: {
+    hex: '#cf9c9c',
+    text: [
+      'Over two years of',
+      'experience teaching',
+      'JavaScript software',
+      'engineering',
+    ],
+  },
+  orange: {
+    hex: '#e5ba9c',
+    text: [
+      'Performed as a',
+      'professional dancer',
+      'in St.Petersburg,',
+      'Berlin, and Seoul',
+    ],
+  },
+  yellow: {
+    hex: '#ddd0af',
+    text: [
+      'President of',
+      'Operation Spark\'s',
+      'Alumni Board',
+    ],
+  },
+  green: {
+    hex: '#bdcec0',
+    text: [
+      'Organizer and',
+      'presenter at local',
+      'tech meetups in',
+      'New Orleans',
+    ],
+  },
+  blue: {
+    hex: '#AADBFF',
+    text: [
+      'Featured in the',
+      'New York Times as',
+      'a woman working',
+      'to elect women',
+    ],
+  },
+  purple: {
+    hex: '#b4a7ca',
+    text: [
+      'Member of',
+      'Tulane University\'s',
+      'Queer Community',
+      'Advisory Board',
+    ],
+  },
+};
+
+function toggleSeptagon() {
+  document.getElementById('septagon').remove();
+  document.getElementsByClassName('about-info-container')[0].style.display = 'initial';
 }
 
-function changeImage (color) {
+function changeBackgroundColor(color) {
+  document.getElementById('about-page').style.backgroundColor = aboutOptions[color].hex;
+}
+
+function changeImage(color) {
   document.getElementById('tidbit-image').style.backgroundImage = `url('/images/about/${color}.jpg')`;
 }
 
-function toggleSeptagon () {
-  document.getElementById('septagon').remove();
-  document.getElementsByClassName('about-info-container')[0].style.display = 'initial';
+function changeTidbitText(color) {
+  const tidbits = document.getElementById('tidbit-container');
+  while (tidbits.firstChild) tidbits.removeChild(tidbits.firstChild);
+  tidbits.children = aboutOptions[color].text.map(line => {
+    const newLine = document.createTextNode(line);
+    const newNode = document.createElement('p');
+    newNode.classList.add('tidbit-line');
+    newNode.appendChild(newLine);
+    tidbits.appendChild(newNode);
+  });
+}
+
+function resetTidbitAnimation() {
+  const el = document.getElementById('tidbit-container');
+  el.classList.remove('animated');
+  void el.offsetWidth;
+  el.classList.add('animated');
+}
+
+function changeAboutOption(color) {
+  const el = document.getElementsByClassName('about-info-container')[0];
+  el.style.display = 'none';
+  changeBackgroundColor(color);
+  changeImage(color);
+  changeTidbitText(color);
+  resetTidbitAnimation();
+  el.style.display = 'initial';
 }
